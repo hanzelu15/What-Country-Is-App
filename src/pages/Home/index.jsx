c0c3ac4fa2481a12774d9be32a6df71e32e02a8f
Hansel <Hanzel_u15@hotmal.com>
2023-04-07 15:26:13 -0600

    fix imgs url

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import logoImage from '../../assets/logoImage.svg';
+import worldImage from '../../assets/worldImage.svg';
 import { MenuButtonComponent } from '../../components/MenuButtonComponent'
 
 export const Home = () => {
@@ -6,8 +8,8 @@ export const Home = () => {
     return (
         <div className=" w-full h-full py-16 bg-no-repeat bg-top bg-contain">
 
-            <img className='w-[250px] m-auto' src="src/assets/logoImage.svg" alt="logo" />
-            <img className='w-[100px] m-auto' src="src/assets/worldImage.svg" alt="logo" />
+            <img className='w-[250px] m-auto' src={logoImage} alt="logo" />
+            <img className='w-[100px] m-auto' src={worldImage} alt="logo" />
             <div className='w-4/5 mt-16 flex flex-col justify-between m-auto'>
                 <MenuButtonComponent url="/PlayGame" text="Play" />
                 <MenuButtonComponent url="/search" text="Learn" />
