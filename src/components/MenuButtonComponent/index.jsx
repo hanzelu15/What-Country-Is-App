a0f80f82dcedd5ad20364cfd08785d6f3fc99fee
Hansel <Hanzel_u15@hotmal.com>
2023-04-06 20:04:27 -0600

    prod files

--- src/components/MenuButtonComponent/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-
-export const MenuButtonComponent = ({ url, text }) => {
-    return (
-        <Link to={url} className='w-full my-5  py-3 px-5 text-gray-900 text-2xl text-center border border-main-green rounded-3xl duration-300 hover:bg-light-green'>{text}</Link>
-    )
-}
